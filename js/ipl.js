3ef30aea5b4d6104cd082fe6fc2b516195a293f8
inkfarer <inkfarer@inkfarer.com>
2020-07-25 09:59:10 +0300

    "Who are we?" section
    technically more than just the page header now...

--- js/ipl.js
+++ js/ipl.js
@@ -4,7 +4,12 @@ const sidebarWidth = '55px';
 
 var mobileMenuShown = false;
 
+
 headerElem.addEventListener('mouseover', () => {
+	// Move over page content if header is hovered on
+	// and we're not on mobile
+
+	// I'm like 90% sure this can't be done with css
 	if (window.innerWidth > 576) {
 		let sidebarExpandedWidth = '160px';
 		pageContent.style.marginLeft = sidebarExpandedWidth;
@@ -12,12 +17,17 @@ headerElem.addEventListener('mouseover', () => {
 });
 
 headerElem.addEventListener('mouseout', () => {
+	// Header closing anim
 	if (window.innerWidth > 576) {
 		pageContent.style.marginLeft = sidebarWidth;
 	}
 });
 
 window.addEventListener('resize', () => {
+	// When the window gets resized, make sure appropriate styles
+	// for the window size are given
+
+	// This is here because CSS media queries don't change styles given out by JavaScript.
 	if (window.innerWidth < 576) {
 		pageContent.style.marginLeft = '0px';
 		if (!mobileMenuShown) {
