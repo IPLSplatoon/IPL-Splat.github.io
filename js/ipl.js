d991053069f44bb5cd9fdf53f76b60f4116bbc5a
inkfarer <inkfarer@inkfarer.com>
2020-07-26 08:05:45 +0300

    Add EJS and make it work
    Index page is ready, just needs our tournaments to not be placeholders

--- js/ipl.js
+++ js/ipl.js
@@ -50,3 +50,46 @@ function toggleMobileMenu() {
 		mobileMenuShown = false;
 	}
 }
+
+const tournaments = [
+	{
+		title: 'Low Ink',
+		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae assumenda placeat harum repellat quaerat optio quidem nostrum voluptas tenetur, laboriosam, voluptatibus laborum eveniet, praesentium debitis?',
+		imagePath: 'images/logo-low-ink.png'
+	},
+	{
+		title: 'Low Ink',
+		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae assumenda placeat harum repellat quaerat optio quidem nostrum voluptas tenetur, laboriosam, voluptatibus laborum eveniet, praesentium debitis?',
+		imagePath: 'images/logo-low-ink.png'
+	},
+	{
+		title: 'Low Ink',
+		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae assumenda placeat harum repellat quaerat optio quidem nostrum voluptas tenetur, laboriosam, voluptatibus laborum eveniet, praesentium debitis?',
+		imagePath: 'images/logo-low-ink.png'
+	},
+	{
+		title: 'Low Ink',
+		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae assumenda placeat harum repellat quaerat optio quidem nostrum voluptas tenetur, laboriosam, voluptatibus laborum eveniet, praesentium debitis?',
+		imagePath: 'images/logo-low-ink.png'
+	},
+	{
+		title: 'Low Ink',
+		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae assumenda placeat harum repellat quaerat optio quidem nostrum voluptas tenetur, laboriosam, voluptatibus laborum eveniet, praesentium debitis?',
+		imagePath: 'images/logo-low-ink.png'
+	}
+];
+
+// Let EJS create the HTML for each element in the above array for us
+let tourneyElements = ejs.render(`
+	<% tournaments.forEach(tournament => { %>
+	<div class="tournament">
+		<img src="<%=tournament.imagePath%>">
+		<div class="tournament-description">
+			<h3><%=tournament.title%></h3>
+			<p><%=tournament.description%></p>
+		</div>
+	</div>
+	<% }) %>
+`, {tournaments: tournaments});
+
+document.querySelector('.tournaments-grid').innerHTML = tourneyElements;
