e927a8e002c5b08d5d04b948b6cd53a13c8db9b0
Vincent Lee <vlee@vlee.me.uk>
2020-07-26 16:38:39 +0100

    Allow Tournament Data to be load from JSON file

--- js/ipl.js
+++ js/ipl.js
@@ -6,90 +6,68 @@ var mobileMenuShown = false;
 
 
 headerElem.addEventListener('mouseover', () => {
-	// Move over page content if header is hovered on
-	// and we're not on mobile
+    // Move over page content if header is hovered on
+    // and we're not on mobile
 
-	// I'm like 90% sure this can't be done with css
-	if (window.innerWidth > 576) {
-		let sidebarExpandedWidth = '160px';
-		pageContent.style.marginLeft = sidebarExpandedWidth;
-	}
+    // I'm like 90% sure this can't be done with css
+    if (window.innerWidth > 576) {
+        let sidebarExpandedWidth = '160px';
+        pageContent.style.marginLeft = sidebarExpandedWidth;
+    }
 });
 
 headerElem.addEventListener('mouseout', () => {
-	// Header closing anim
-	if (window.innerWidth > 576) {
-		pageContent.style.marginLeft = sidebarWidth;
-	}
+    // Header closing anim
+    if (window.innerWidth > 576) {
+        pageContent.style.marginLeft = sidebarWidth;
+    }
 });
 
 window.addEventListener('resize', () => {
-	// When the window gets resized, make sure appropriate styles
-	// for the window size are given
+    // When the window gets resized, make sure appropriate styles
+    // for the window size are given
 
-	// This is here because CSS media queries don't change styles given out by JavaScript.
-	if (window.innerWidth < 576) {
-		pageContent.style.marginLeft = '0px';
-		if (!mobileMenuShown) {
-			headerElem.style.height = '45px';
-		}
-	} else {
-		pageContent.style.marginLeft = sidebarWidth;
-		headerElem.style.height = '100%';
-	}
+    // This is here because CSS media queries don't change styles given out by JavaScript.
+    if (window.innerWidth < 576) {
+        pageContent.style.marginLeft = '0px';
+        if (!mobileMenuShown) {
+            headerElem.style.height = '45px';
+        }
+    } else {
+        pageContent.style.marginLeft = sidebarWidth;
+        headerElem.style.height = '100%';
+    }
 });
 
 function toggleMobileMenu() {
-	if (!mobileMenuShown) {
-		headerElem.style.height = '100%';
-		headerElem.style.overflowY = 'visible'
-		mobileMenuShown = true;
-	} else {
-		headerElem.style.height = '45px';
-		headerElem.style.overflowY = 'hidden'
-		mobileMenuShown = false;
-	}
+    if (!mobileMenuShown) {
+        headerElem.style.height = '100%';
+        headerElem.style.overflowY = 'visible'
+        mobileMenuShown = true;
+    } else {
+        headerElem.style.height = '45px';
+        headerElem.style.overflowY = 'hidden'
+        mobileMenuShown = false;
+    }
 }
 
-const tournaments = [
-	{
-		title: 'Low Ink',
-		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae assumenda placeat harum repellat quaerat optio quidem nostrum voluptas tenetur, laboriosam, voluptatibus laborum eveniet, praesentium debitis?',
-		imagePath: 'images/logo-low-ink.png'
-	},
-	{
-		title: 'Low Ink',
-		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae assumenda placeat harum repellat quaerat optio quidem nostrum voluptas tenetur, laboriosam, voluptatibus laborum eveniet, praesentium debitis?',
-		imagePath: 'images/logo-low-ink.png'
-	},
-	{
-		title: 'Low Ink',
-		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae assumenda placeat harum repellat quaerat optio quidem nostrum voluptas tenetur, laboriosam, voluptatibus laborum eveniet, praesentium debitis?',
-		imagePath: 'images/logo-low-ink.png'
-	},
-	{
-		title: 'Low Ink',
-		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae assumenda placeat harum repellat quaerat optio quidem nostrum voluptas tenetur, laboriosam, voluptatibus laborum eveniet, praesentium debitis?',
-		imagePath: 'images/logo-low-ink.png'
-	},
-	{
-		title: 'Low Ink',
-		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae assumenda placeat harum repellat quaerat optio quidem nostrum voluptas tenetur, laboriosam, voluptatibus laborum eveniet, praesentium debitis?',
-		imagePath: 'images/logo-low-ink.png'
-	}
-];
-
-// Let EJS create the HTML for each element in the above array for us
-let tourneyElements = ejs.render(`
-	<% tournaments.forEach(tournament => { %>
-	<div class="tournament">
-		<img src="<%=tournament.imagePath%>">
-		<div class="tournament-description">
-			<h3><%=tournament.title%></h3>
-			<p><%=tournament.description%></p>
-		</div>
-	</div>
-	<% }) %>
-`, {tournaments: tournaments});
-
-document.querySelector('.tournaments-grid').innerHTML = tourneyElements;
+fetch('data/tournaments.json')
+    .then((response) => {
+        return response.json()
+    })
+    .then((data) => {
+        // Work with JSON data here
+        console.log(data)// Let EJS create the HTML for each element in the above array for us
+        let tourneyElements = ejs.render(`
+	        <% tournaments.forEach(tournament => { %>
+	        <div class="tournament">
+		        <img src="<%=tournament.imagePath%>">
+		        <div class="tournament-description">
+			        <h3><%=tournament.title%></h3>
+			        <p><%=tournament.description%></p>
+		        </div>
+	        </div>
+	        <% }) %>
+        `, {tournaments: data});
+        document.querySelector('.tournaments-grid').innerHTML = tourneyElements;
+    })
