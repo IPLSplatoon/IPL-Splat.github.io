672b5b5a2f759ae1a873f40892327205305307df
inkfarer <inkfarer@inkfarer.com>
2021-04-10 11:59:24 +0300

    Tweak tournaments.js

--- js/tournaments.js
+++ js/tournaments.js
@@ -1,21 +1,27 @@
-fetch('data/tournaments.json')
-    .then((response) => {
-        return response.json()
-    })
-    .then((data) => {
-        // Work with JSON data here
-        // Let EJS create the HTML for each element in the above array for us
-        let tourneyElements = ejs.render(`
-	        <% tournaments.forEach(tournament => { %>
-	        <div class="card">
-		        <img src="<%=tournament.imagePath%>" alt="Icon for <%=tournament.title%>">
-		        <div class="card-text">
-			        <h3><%=tournament.title%></h3>
-			        <p><%=tournament.description%></p>
-		        </div>
-	        </div>
-	        <% }) %>
-        `, {tournaments: data});
-        document.querySelector('.tournaments-grid').innerHTML += tourneyElements;
-        new Colcade('.tournaments-grid', {columns: '.col', items: '.card'});
-    })
+const tournamentGridElem = document.querySelector('.tournaments-grid');
+
+function fillTournamentGrid() {
+    fetch('data/tournaments.json')
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            tournamentGridElem.innerHTML = `
+            <div class="col"></div>
+            <div class="col col-2"></div>
+            <div class="col col-3"></div>`;
+            tournamentGridElem.innerHTML += ejs.render(`
+                <% tournaments.forEach(tournament => { %>
+                <div class="card">
+                    <img src="<%=tournament.imagePath%>" alt="Icon for <%=tournament.title%>">
+                    <div class="card-text">
+                        <h3><%=tournament.title%></h3>
+                        <p><%=tournament.description%></p>
+                    </div>
+                </div>
+                <% }) %>
+            `, {tournaments: data});
+
+            new Colcade(tournamentGridElem, {columns: '.col', items: '.card'});
+        });
+}
